2f7c54ee21054d7b1a498f6dc310184bf31faef8
argierdfj <argierincon@gmail.com>
2019-12-28 18:42:58 -0500

    Cambio nombre de archivo, y funcionalidad.

--- trivia.js
+++ trivia.js
@@ -14,15 +14,84 @@ botonIniciar.addEventListener('click', () => {
     varMensajeUsuario.innerText = `Hola ${varNombre.toUpperCase()}, veamos cuánto conoces del Perú.`
 });
 
+// Funcionalidad trivia Perú
+
+const botonPeru = document.getElementById('peru');
+
+botonPeru.addEventListener('click', () => {
+    const varPantallaDeOpciones = document.getElementById('pantallaDeOpciones');
+    const varPeru1 = document.getElementById('peru1');
+
+    varPantallaDeOpciones.classList.add('ocultar');
+    varPeru1.classList.remove('ocultar');
+});
+
+const botonSiguiente1Peru = document.getElementById('siguiente1Peru');
+
+botonSiguiente1Peru.addEventListener('click', () => {
+    const varPeru1 = document.getElementById('peru1');
+    const varPeru2 = document.getElementById('peru2');
+
+    varPeru1.classList.add('ocultar');
+    varPeru2.classList.remove('ocultar');
+});
+
+const botonSiguiente2Peru = document.getElementById('siguiente2Peru');
+
+botonSiguiente2Peru.addEventListener('click', () => {
+    const varPeru2 = document.getElementById('peru2');
+    const varPeru3 = document.getElementById('peru3');
+
+    varPeru2.classList.add('ocultar');
+    varPeru3.classList.remove('ocultar');
+});
+
+const botonResultadoPeru = document.getElementById('resultadoPeru');
+
+botonResultadoPeru.addEventListener('click', () => {
+    const varPeru3 = document.getElementById('peru3');
+    const varResultadoOpciones = document.getElementById('resultadoOpciones');
+    const varNombre = document.getElementById('nombre');
+    const varUsuario = varNombre.value;
+    const varMensajeResultado = document.getElementById('mensajeResultado');
+    const preg1Peru = document.trivia1.preg1Peru.value;
+    const preg2Peru = document.trivia1.preg2Peru.value;
+    const preg3Peru = document.trivia1.preg3Peru.value;
+
+    varPeru3.classList.add('ocultar');
+    varResultadoOpciones.classList.remove('ocultar');
+    
+    let correctas = 0;
+
+    if (preg1Peru === 'Cuzco') {
+        correctas++;
+    }
+
+    if (preg2Peru === 'Vicuña') {
+        correctas++;
+    }
+
+    if (preg3Peru === 'Quechua') {
+        correctas++;
+    }
+
+    if (correctas === 1) {
+        varMensajeResultado.innerText = `${varUsuario} Tienes ${correctas} acierto`
+    } else {
+        varMensajeResultado.innerText = `${varUsuario} Tienes ${correctas} aciertos`
+    }
+});
+
+// Funcionalidad trivia Gastronomía
 const botonGastronomia = document.getElementById('gastronomia');
 
 botonGastronomia.addEventListener('click', () => {
-    const varPantallaDeOpcines = document.getElementById('pantallaDeOpciones');
+    const varPantallaDeOpciones = document.getElementById('pantallaDeOpciones');
     const varGastronomia1 = document.getElementById('gastronomia1');
 
-    varPantallaDeOpcines.classList.add('ocultar');
+    varPantallaDeOpciones.classList.add('ocultar');
     varGastronomia1.classList.remove('ocultar');
-})
+});
 
 const botonSiguiente1Gastronomia = document.getElementById('siguiente1Gastronomia');
 
@@ -55,36 +124,33 @@ botonResultadoGastronomia.addEventListener('click', () => {
     const varResultadoOpciones = document.getElementById('resultadoOpciones');
     const varNombre = document.getElementById('nombre');
     const varUsuario = varNombre.value;
-    const varmensajeResultado = document.getElementById('mensajeResultado');
-    const preg1gastronomia = document.trivia1.preg1gastronomia.value;
-    const preg2gastronomia = document.trivia1.preg2gastronomia.value;
-    const preg3gastronomia = document.trivia1.preg3gastronomia.value;
+    const varMensajeResultado = document.getElementById('mensajeResultado');
+    const preg1Gastronomia = document.trivia2.preg1Gastronomia.value;
+    const preg2Gastronomia = document.trivia2.preg2Gastronomia.value;
+    const preg3Gastronomia = document.trivia2.preg3Gastronomia.value;
 
     varGastronomia3.classList.add('ocultar');
     varResultadoOpciones.classList.remove('ocultar');
 
     let correctas = 0;
 
-    if (preg1gastronomia === 'Pachamanca') {
+    if (preg1Gastronomia === 'Pachamanca') {
         correctas++;
     }
 
-    if (preg2gastronomia === 'Papa seca y carne de cerdo') {
+    if (preg2Gastronomia === 'Papa seca y carne de cerdo') {
         correctas++;
     }
 
-    if (preg3gastronomia === 'Costa') {
+    if (preg3Gastronomia === 'Costa') {
         correctas++;
     }
 
     if (correctas === 1) {
-        varmensajeResultado.innerText = `${varUsuario} Tienes ${correctas} acierto`
+        varMensajeResultado.innerText = `${varUsuario} Tienes ${correctas} acierto`
     } else {
-        varmensajeResultado.innerText = `${varUsuario} Tienes ${correctas} aciertos`
+        varMensajeResultado.innerText = `${varUsuario} Tienes ${correctas} aciertos`
     }
-
-   // varmensajeResultado.innerText = ` ${varUsuario} Tienes ${correctas} aciertos`
-
 })
 
 
